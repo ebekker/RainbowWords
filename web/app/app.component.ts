e2cce9d27da63c6b441a539aa0090126e4f01f64
Eugene Bekker <ebekker@powervision.com>
2016-01-29 13:41:57 -0500

    Splitting out template and styles

--- web/app/app.component.ts
+++ web/app/app.component.ts
@@ -5,74 +5,8 @@ import {WordsService} from './words.service';
 
 @Component({
     selector: 'my-app',
-    template: `
-        <h1>{{title}}</h1>
-        <ul>
-            <li>Randomize</li>
-            <li>Hide Color</li>
-            <li>Repeat</li>
-        </ul>
-        
-        <ul>
-            <li>{{hasPrev}}</li>
-            <li>{{hasNext}}</li>
-            <li>{{words}}</li>
-        </ul>
-        
-        <ul>
-            <li class="currentWord">Word = {{currentWord}}</li>
-            <li class="prev" *ngIf="hasPrev" (click)="onPrev()">Prev</li>
-            <li class="next" *ngIf="hasNext" (click)="onNext()">Next</li>
-        </ul>
-        `,
-    styles: [`
-        .selected {
-            background-color:  #CFD8DC !important;
-            color: white;
-        }
-        .heroes {
-            margin: 0 0 2em 0;
-            list-style-type: none;
-            padding: 0;
-            width: 10em;
-        }
-        .heroes li {
-            cursor: pointer;
-            position: relative;
-            left: 0;
-            background-color: #EEE;
-            margin: .5em;
-            padding: .3em 0em;
-            height: 1.6em;
-            border-radius: 4px;
-        }
-        .heroes li.selected:hover {
-            color: white;
-        }
-        .heroes li:hover {
-            color: #607D8B;
-            background-color: #EEE;
-            left: .1em;
-        }
-        .heroes .text {
-            position: relative;
-            top: -3px;
-        }
-        .heroes .badge {
-            display: inline-block;
-            font-size: small;
-            color: white;
-            padding: 0.8em 0.7em 0em 0.7em;
-            background-color: #607D8B;
-            line-height: 1em;
-            position: relative;
-            left: -1px;
-            top: -4px;
-            height: 1.8em;
-            margin-right: .8em;
-            border-radius: 4px 0px 0px 4px;
-        }
-    `],
+    templateUrl: 'app/app.component.html',
+    styleUrls: ['app/app.component.css'],
     //directives: [HeroDetailComponent],
     providers: [WordsService],
 })
