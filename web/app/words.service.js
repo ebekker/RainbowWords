31cfb42f8a07b3f446034f9f84f24a73361ff179
Eugene Bekker <ebekker@powervision.com>
2016-01-31 13:42:01 -0500

    Switching to a stable source of words
    The GIST always changed with every revision, but snipt's URL is stable
    even after edits

--- web/app/words.service.js
+++ web/app/words.service.js
@@ -29,7 +29,8 @@ System.register(['angular2/core', 'angular2/http', './mock-words', 'rxjs/Observa
             DEFAULT_WORDS_SOURCE = 
             //"https://gist.githubusercontent.com/ebekker/e1fc9b6c928bfdc548ac/raw/f364d986cd64532a7763f26f68f869ebb2490090/ces-mccullough.words.json";
             //"https://gist.githubusercontent.com/ebekker/e1fc9b6c928bfdc548ac/raw/14d11dbb64e421a10a29a7822a21d39127973431/ces-mccullough.words.json";
-            "https://gist.githubusercontent.com/ebekker/e1fc9b6c928bfdc548ac/raw/f36122cc58fd0e9f9fd65a2403b193ec94a47161/ces-mccullough.words.json";
+            //"https://gist.githubusercontent.com/ebekker/e1fc9b6c928bfdc548ac/raw/f36122cc58fd0e9f9fd65a2403b193ec94a47161/ces-mccullough.words.json";
+            "https://snipt.net/raw/e47b8c934cad4ad18b091347eb3e6737/";
             WordsService = (function () {
                 function WordsService(_http) {
                     this._http = _http;
--- web/app/words.service.ts
+++ web/app/words.service.ts
@@ -10,7 +10,8 @@ import 'rxjs/Rx';
 var DEFAULT_WORDS_SOURCE =
       //"https://gist.githubusercontent.com/ebekker/e1fc9b6c928bfdc548ac/raw/f364d986cd64532a7763f26f68f869ebb2490090/ces-mccullough.words.json";
       //"https://gist.githubusercontent.com/ebekker/e1fc9b6c928bfdc548ac/raw/14d11dbb64e421a10a29a7822a21d39127973431/ces-mccullough.words.json";
-        "https://gist.githubusercontent.com/ebekker/e1fc9b6c928bfdc548ac/raw/f36122cc58fd0e9f9fd65a2403b193ec94a47161/ces-mccullough.words.json";
+      //"https://gist.githubusercontent.com/ebekker/e1fc9b6c928bfdc548ac/raw/f36122cc58fd0e9f9fd65a2403b193ec94a47161/ces-mccullough.words.json";
+        "https://snipt.net/raw/e47b8c934cad4ad18b091347eb3e6737/";
 
 @Injectable()
 export class WordsService {
